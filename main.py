651b66ef80ef06796407f72b1ba10237ee6eea4e
adioiz <101355773+adioiz@users.noreply.github.com>
2022-04-06 19:47:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,14 +22,11 @@ while len(correct_guesses) < 50:
     ans_state = screen.textinput(title=f"{len(correct_guesses)}/50 states", prompt="Enter state:").title()
 
     if ans_state == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in correct_guesses:
-                missing_states.append(state)
+        missing_states = [state for state in states_list if state not in correct_guesses]
         new_data = pandas.DataFrame(missing_states)
-
         new_data.to_csv('missing states.csv')
         break
+        
     if ans_state in states_list:
         state = data[data.state == ans_state]
         if not state.state.item() in correct_guesses:
